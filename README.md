b6f79df8f641038c653f3365fe0ae0f52e23ccd8
leonardoheringer <heringerleonardo560@gmail.com>
2024-10-03 16:18:58 -0300

    Automação sistema Hidráulico

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Automa-oHidr-ulica
+# Simulador Hidráulico
+
+## Descrição
+
+Este projeto é um simulador de um sistema hidráulico, que utiliza uma lógica simples para representar o funcionamento de boias e registros. O simulador gera aleatoriamente o estado de três boias (A, B e C) e, com base nesses estados, exibe mensagens indicando ações a serem tomadas no sistema, como ligar registros ou solicitar técnicos.
+
+## Funcionalidades
+
+- Simulação aleatória do estado de três boias:
+  - Boia A
+  - Boia B
+  - Boia C
+- Exibição do estado de cada boia (ligada ou desligada).
+- Ações baseadas no estado das boias, incluindo:
+  - Ligar registros
+  - Ligar bombas
+  - Notificar erro em sensores
+- Opção para reiniciar a simulação.
+
+## Tecnologias Utilizadas
+
+- C#
+- .NET
+
+## Como Usar
+
+1. Clone o repositório ou baixe os arquivos.
+2. Abra o projeto no Visual Studio ou em outro IDE de sua escolha que suporte C#.
+3. Compile e execute o programa.
+4. Siga as instruções exibidas no console para interagir com o simulador.
+
+## Demonstração
+Demonstração algoritmo em uso.
+<img src="Demonstração.png" alt="Print execução">
+
+
+## Contribuições
+
+Contribuições são bem-vindas! Se você deseja sugerir melhorias ou corrigir erros, sinta-se à vontade para abrir uma issue ou criar um pull request.
+
---
+++ Simulador/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+Random randNum = new Random();
+bool repetir = true;
+while (repetir == true)
+{
+int b1 = randNum.Next(0, 2);
+int b2 = randNum.Next(0, 2);
+int b3 = randNum.Next(0, 2);
+Console.Clear();
+ Console.WriteLine("-------BEM VINDO SIMULADOR HIDRÁULICO-------");
+ Console.WriteLine("Simula a automação de um sistema hidráulico");
+    if (b1 == 1)
+    {
+        Console.WriteLine("Boia A está lígada!");
+    }
+    else
+    {
+        Console.WriteLine("Boia A está Desligada!");
+    }
+    if (b2 == 1)
+    {
+        Console.WriteLine("Boia B está lígada!");
+    }
+    else
+    {
+        Console.WriteLine("Boia B está Desligada!");
+    }
+    if (b3 == 1)
+    {
+        Console.WriteLine("Boia C está lígada!");
+    }
+    else
+    {
+        Console.WriteLine("Boia C está Desligada!");
+    }
+
+    if (b1 == 0 && b2 == 0 && b3 == 0)
+    {
+        Console.WriteLine("Ligar Registro!");
+    }
+    if (b1 == 0 && b2 == 0 && b3 == 1)
+    {
+        Console.WriteLine("Ligar Registro!");
+    }
+    if (b1 == 0 && b2 == 1 && b3 == 0)
+    {
+        Console.WriteLine("Erro Sensor!");
+        Console.WriteLine("Solicitar Técnico!");
+    }
+
+    if (b1 == 0 && b2 == 1 && b3 == 1)
+    {
+        Console.WriteLine("Erro Sensor!");
+        Console.WriteLine("Solicitar Técnico!");
+    }
+    if (b1 == 1 && b2 == 0 && b3 == 0)
+    {
+        Console.WriteLine("Ligar Registro!");
+        Console.WriteLine("Ligar Bomba!");
+    }
+    if (b1 == 1 && b2 == 0 && b3 == 1)
+    {
+        Console.WriteLine("Ligar Registro!");
+    }
+    if (b1 == 1 && b2 == 1 && b3 == 0)
+    {
+        Console.WriteLine("Ligar Bomba!");
+    }
+    if (b1 == 1 && b2 == 1 && b3 == 1)
+    {
+        Console.WriteLine("Manter bomba desligada!");
+        Console.WriteLine("Manter registro copasa desligado!");
+    }
+    Console.WriteLine("Deseja simular novamente? (s/n)");
+
+    var continuar = Console.ReadLine();
+
+    if (continuar?.ToLower() != "s")
+    {
+        Console.WriteLine("Obrigado!");
+        repetir = false;
+    }
+}
